a06611f916ca251e6f3217184f1818a22538dab3
Jason Souryamath <jasonsouryamath@yahoo.com>
2017-05-08 16:06:04 -0700

    testing theapi

---
+++ angularapp/src/app/search-users/search-users.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Input } from '@angular/core';
+//inject service into angular4 component
+import { SearchUsersService } from '../search-users.service';
+@Component({
+  selector: 'app-search-users',
+  templateUrl: './search-users.component.html',
+  styleUrls: ['./search-users.component.css']
+})
+export class SearchUsersComponent implements OnInit {
+  place: string;
+  language: string;
+  results: any[] = []; // This will hold the data coming from the service
+  selected: boolean = false; // Flag to check if a user is clicked or not
+  selectedUser: any; // presently Selected user details
+  error_text: string = ""; // So called error reporing text to the end user
+
+  constructor(private searchService: SearchUsersService) {}
+  
+  ngOnInit() {}
+
+
+  search(place: string, language: string) {
+    this.selected = false;
+    this.error_text = "";
+    if (place || language) {
+      this.place = place;
+      this.language = language;
+      this.searchService.getUsersByPlaceAndLanguage(place, language).subscribe(
+        users => {
+          this.results = users;
+        },
+        error => {
+          this.results = [];
+          this.error_text = "Sorry! No Users found. Try again";
+          console.error(error);
+
+        }
+      )
+    }
+  }
+  getDetails(username: string) {
+    this.searchService.getDetailsByUserName(username).subscribe(
+      userDatils => {
+        this.selectedUser = userDatils;
+        this.selected = true;
+      },
+      error => {
+        this.selected = false;
+        console.error(error);
+      }
+    )
+  }
+
+}
+
